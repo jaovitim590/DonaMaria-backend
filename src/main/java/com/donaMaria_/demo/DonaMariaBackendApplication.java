a7c2da0aee43e7e09b5a2958429cd1da63909b77
jaovitim <joaorodriguesjv590@gmail.com>
2026-02-24 13:41:31 -0300

    first commit

---
+++ src/main/java/com/donaMaria_/demo/DonaMariaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.donaMaria_.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonaMariaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonaMariaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/donaMaria_/demo/DonaMariaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donaMaria_.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonaMariaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
